09184bf89ee7f351452ca29eac41d6de963720c2
alisondraV <alice.7414122013@gmail.com>
2023-05-03 16:52:39 -0400

    Update main-service to be a micro-service that communicates through RMQ

--- main-service/src/main.ts
+++ main-service/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'main_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
